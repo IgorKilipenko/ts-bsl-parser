ef17bb1a3445c62e8cab410f3cb57ed77fa155b2
IgorKilipenko <igorkili@mail.ru>
2024-01-15 13:16:48 +0700

    add createTokenStream to BslGrammarLexer

--- src/bslGrammarLexer.ts
+++ src/bslGrammarLexer.ts
@@ -1,3 +1,10 @@
-import { BSLLexer as BslGrammarLexer } from "./antlr/generated/BSLLexer";
+import { CommonTokenStream, CharStreams } from "antlr4ng";
+import { BSLLexer as BSLLexerBase } from "./antlr/generated/BSLLexer";
 
-export { BslGrammarLexer };
+export class BslGrammarLexer extends BSLLexerBase {
+    public static createTokenStream(data: string): CommonTokenStream {
+        const chars = CharStreams.fromString(data);
+        const lexer = new BslGrammarLexer(chars);
+        return new CommonTokenStream(lexer);
+    }
+}
